108bb7c38667a793a4b910384f073ce27e3bcef0
Rongye Shi <rongyeshi@gmail.com>
2018-04-12 20:51:58 -0400

    first commit

--- Double_DQN_SpaceInvaders.py
+++ Double_DQN_SpaceInvaders.py
@@ -47,28 +47,21 @@ class QNetwork():
         if Flag==0:
             
             
-            #inputs = Input(shape = (210,160,4))#input layer, the shape is 4*84*84
-            #resize = Lambda(lambda inputs: K.resize_images(inputs, 84, 84, data_format = "channels_last"))(inputs)#resize image into (84,84,4)
             inputs = Input(shape = (84,84,4))
             net = Conv2D(kernel_size=8, strides=4, filters=16, padding='same',
                          activation='relu')(inputs)#(resize)
-            #net = MaxPooling2D(pool_size=2, strides=2)(net)
-                    
-            #net = MaxPooling2D(pool_size=2, strides=2)(net)####
             net = Flatten()(net)
             net = Dense(8, activation='relu')(net)
         
             self.outputs = Dense(self.Num_output, activation='linear')(net)
         if Flag==1:
-            #inputs = Input(shape = (210,160,4))#input layer, the shape is 4*84*84
-            #resize = Lambda(lambda inputs: K.resize_images(inputs, 84, 84, data_format = "channels_last"))(inputs)#resize image into (84,84,4)
+            
             inputs = Input(shape = (84,84,4))
             net = Conv2D(kernel_size=8, strides=4, filters=16, padding='same',
                          activation='relu')(inputs)
             
             net = Conv2D(kernel_size=4, strides=2, filters=32, padding='same',
                          activation='relu')(net)
-            #net = Conv2D(kernel_size=3, strides=1, filters=64, padding='same',activation='relu')(net)
             
             net = Flatten()(net)
             net = Dense(256, activation='relu')(net)
@@ -76,17 +69,14 @@ class QNetwork():
             self.outputs = Dense(self.Num_output, activation='linear')(net)
         
         if Flag==2:
-            #inputs = Input(shape = (210,160,4))#input layer, the shape is 4*84*84
-            #resize = Lambda(lambda inputs: K.resize_images(inputs, 84, 84, data_format = "channels_last"))(inputs)#resize image into (84,84,4)
+            
             inputs = Input(shape = (input_dim,input_dim,4))
             net = Conv2D(kernel_size=8, strides=4, filters=32, padding='same',
                          activation='relu')(inputs)
-            #net = MaxPooling2D(pool_size=2, strides=2)(net)
             net = Conv2D(kernel_size=4, strides=2, filters=64, padding='same',
                          activation='relu')(net)
             net = Conv2D(kernel_size=3, strides=1, filters=64, padding='same',
                          activation='relu')(net)
-            #net = MaxPooling2D(pool_size=2, strides=2)(net)####
             net = Flatten()(net)
             net = Dense(512, activation='relu')(net)
         
@@ -94,33 +84,27 @@ class QNetwork():
         
         
         
-        #self.model = Model(inputs = inputs, outputs = self.outputs)
+        
         self.model = Model(inputs = inputs, outputs = self.outputs)
-        
-        #optmiz = keras.optimizers.RMSprop(lr=self.learning_rate)
-        
         self.model.compile(loss='mse', optimizer=Adam(lr=Learning_rate))
-        #self.model.compile(optimizer=optmiz,loss='mean_squared_error')
         print("network constructed")
-        #plot_model(self.model, to_file='model.png')
         
         self.path_model = 'model_linear.h5'
         self.path_weight = 'weight_linear.h5'
         
 
     def save_model_weights(self, suffix):
-        # Helper function to save your model / weights. 
+        
         self.model.save(self.path_model)
         self.model.save_weights(self.path_weight)
 
     def load_model(self, model_file):
-        # Helper function to load an existing model.
+        
         self.model=load_model(model_file)
         
 
     def load_model_weights(self,weight_file):
-        # Helper funciton to load model weights. 
-        #self.model.load_weights(self.path_weight)
+        
         pass
 
 
@@ -129,11 +113,8 @@ class QNetwork():
 
 class Replay_Memory():
     
-    #def __init__(self, memory_size=500000, burn_in=100000):#000):
-    #def __init__(self, memory_size=1000000, burn_in=50000):
     def __init__(self, memory_size=110000, burn_in=30000):
-    #def __init__(self, memory_size=200000, burn_in=90000):
-
+    
         # The memory essentially stores transitions recorder from the agent
         # taking actions in the environment.
 
@@ -146,7 +127,6 @@ class Replay_Memory():
 
     def sample_batch(self, batch_size=32):
         # This function returns a batch of randomly sampled transitions - i.e. state, action, reward, next state, terminal flag tuples. 
-        # You will feed this to your model to train.
         minibatch = random.sample(self.memory, batch_size)
         return minibatch
 
@@ -171,15 +151,10 @@ class DQN_Agent():
 
     def __init__(self, environment_name, render=False):
 
-        # Create an instance of the network itself, as well as the memory. 
-        # Here is also a good place to set environmental parameters,
-        # as well as training parameters - number of episodes / iterations, etc. 
-
-        #pass
+        
         self.env = gym.make(environment_name)
         self.Num_input = len(self.env.observation_space.high)
         self.Num_output = self.env.action_space.__dict__['n']#get the number of action
-        #print(environment_name,self.Num_input,self.Num_output)
         self.if_render = render
         self.environment_name = environment_name
         
@@ -200,8 +175,7 @@ class DQN_Agent():
         
 
     def epsilon_greedy_policy(self, q_values, epsilon):
-        # Creating epsilon greedy probabilities to sample from.             
-        #pass
+        
         non_max = epsilon/self.Num_output
         max_greedy = 1 - epsilon + epsilon/self.Num_output
         max_inx = np.argmax(q_values)
@@ -214,8 +188,7 @@ class DQN_Agent():
         
 
     def greedy_policy(self, q_values):
-        # Creating greedy policy for test time. 
-        #pass
+        
         return np.argmax(q_values)
 
     def train(self):
@@ -223,11 +196,7 @@ class DQN_Agent():
         # If training without experience replay_memory, then you will interact with the environment 
         # in this function, while also updating your network parameters. 
 
-        # If you are using a replay memory, you should interact with environment here, and store these 
-        # transitions to memory, while also updating your model.
-        #pass
         
-        #self.QNet.load_model('save/MountainCar-v0DRM.h5')
         itera = 0
         episode = 0
         decay = (0.8 - 0.1)/300000
@@ -240,7 +209,6 @@ class DQN_Agent():
         old_Model2 = QNetwork(self.environment_name)
         model_list = [1,1]#[old_model, curr_model]
         model_list2 = [1,1]
-        #model_list = [self.QNet.model, self.QNet.model]#[old_model, curr_model]
         average_reward_episode = []
         average_step = []
         average_loss = []
@@ -288,16 +256,12 @@ class DQN_Agent():
                 
                 action_cont += 1
                 nextstate, reward, is_terminal, debug_info = self.env.step(cur_action)
-                #reward = min(reward,1.0)
-                #reward = max(-1, min(reward, 1))
-        
                 
-        
                 frames_4.append(rgb2gray(nextstate))
                 reward_list.append(reward)
             
                 if len(frames_4) > 4:
-                    #print(len(frames_4),action_cont)
+                    
                     epsilon = max(epsilon-decay, 0.1)
                     itera += 1
                     
@@ -313,17 +277,15 @@ class DQN_Agent():
                     
                     if len(state_buff)>1:# we save new transition into the memory
                         old_state_stack = state_buff[0]#S
-                        #self.replay_memory.append([old_state_stack,cur_action,sum(reward_list),state_stack,is_terminal]) #state, action, reward, next state, terminal flag tuples.
+                        
                         self.replay_memory.append([old_state_stack,cur_action,reward,state_stack,is_terminal]) #state, action, reward, next state, terminal flag tuples.
                         state_buff = state_buff[1:]
                         reward_list = reward_list[1:]
                         
                         #-------------here to find out next action given current state_stack A'
-                        #print("after new network")
                         
                         q_values = self.QNet.model.predict(x = np.expand_dims(state_stack, axis=0))
                         q_values2 = self.QNet2.model.predict(x = np.expand_dims(state_stack, axis=0))
-                        
                         
                         
                         if action_cont%4==0:
@@ -339,17 +301,11 @@ class DQN_Agent():
                             
                             for i in list(range(0,len(minibatch))):
                                 state_t, action_t,reward_t, nextstate_t, terminal_t = minibatch[i]
-                            
                                 inputs[i] = state_t
-                            
-                            
                                 targets[i] = self.QNet.model.predict(x = np.expand_dims(state_t, axis=0))[0]
-                                #Q_sa = self.QNet.model.predict(x = np.reshape(nextstate_t, (1, self.Num_input)))[0]
                                 Q_sa = self.QNet.model.predict(x = np.expand_dims(nextstate_t, axis=0))[0]#use previous weight for stability
                                 Q_sa2= old_Model2.model.predict(x = np.expand_dims(nextstate_t, axis=0))[0][np.argmax(Q_sa)]
                                 
-                            
-                    
                                 if terminal_t:
                                     targets[i, action_t]= reward_t
                                 if not terminal_t:
@@ -373,7 +329,7 @@ class DQN_Agent():
                             
                             
                                 targets[i] = self.QNet2.model.predict(x = np.expand_dims(state_t, axis=0))[0]
-                                #Q_sa = self.QNet.model.predict(x = np.reshape(nextstate_t, (1, self.Num_input)))[0]
+                                
                                 Q_sa2 = self.QNet2.model.predict(x = np.expand_dims(nextstate_t, axis=0))[0]#use previous weight for stability
                                 Q_sa= old_Model.model.predict(x = np.expand_dims(nextstate_t, axis=0))[0][np.argmax(Q_sa2)]
                                 
@@ -501,13 +457,10 @@ class DQN_Agent():
             
 
             while True:
-                #itera += 1
-                #input_state_cur2 = np.reshape(cur_state2, (1, self.Num_input))#S
+                
                 
                 action_cont += 1
                 nextstate2, reward2, is_terminal2, debug_info2 = self.env.step(cur_action2)#S'
-                #reward2 = min(reward2,1.0)
-                
                 frames_4.append(rgb2gray(nextstate2))
                 
                 
@@ -521,14 +474,10 @@ class DQN_Agent():
                 if len(frames_4) > 4:
                     
                     state_stack = np.asarray(frames_4[:-1])
-                    #frames_4 = [frames_4[-1]]
+                    
                     frames_4 = frames_4[1:]
                     state_stack = np.swapaxes(state_stack,0,2)
                     state_stack = np.swapaxes(state_stack,0,1)#S'
-                    
-                    
-                    
-                    #q_values = self.QNet.model.predict(x = np.expand_dims(state_stack, axis=0))
                     
                     q_values = self.QNet.model.predict(x = np.expand_dims(state_stack, axis=0))
                     q_values2 = self.QNet2.model.predict(x = np.expand_dims(state_stack, axis=0))
@@ -593,7 +542,6 @@ class DQN_Agent():
                 
                 action_cont += 1
                 nextstate2, reward2, is_terminal2, debug_info2 = self.env.step(cur_action2)#S'
-                #reward2 = min(reward2,1.0)
                 self.env.render()
                 frames_4.append(rgb2gray(nextstate2))
                 
@@ -612,27 +560,20 @@ class DQN_Agent():
                 if len(frames_4) > 4:
                     
                     state_stack = np.asarray(frames_4[:-1])
-                    #frames_4 = [frames_4[-1]]
                     frames_4 = frames_4[1:]
                     state_stack = np.swapaxes(state_stack,0,2)
                     state_stack = np.swapaxes(state_stack,0,1)#S'
-                    
-                    
                     
                     q_values = self.QNet.model.predict(x = np.expand_dims(state_stack, axis=0))
                     q_values2 = self.QNet2.model.predict(x = np.expand_dims(state_stack, axis=0))
                                     
                     
                     if action_cont%1 == 0:
-                        #cur_action2 = self.greedy_policy(np.add(q_values[0], q_values2[0]))# A'
+                        
                         cur_action2 = self.epsilon_greedy_policy(np.add(q_values[0], q_values2[0]), epsilon = 0)
                         action_cont = 0
                     
                     
-                    #q_values = self.QNet.model.predict(x = np.expand_dims(state_stack, axis=0))
-                    #cur_action2 = self.greedy_policy(q_values[0])# A'
-                    
-                                        
                 
                 total_reward2 += discount2 * reward2
                 #discount *= gamma
@@ -685,20 +626,15 @@ class DQN_Agent():
                 
                 action_cont += 1
                 nextstate, reward, is_terminal, debug_info = self.env.step(cur_action)
-                
-                #reward = max(-1, min(reward, 1))
-                
-        
                 frames_4.append(rgb2gray(nextstate))
             
                 if len(frames_4) > 4:
                     
                     state_stack = np.asarray(frames_4[:-1])
-                    #frames_4 = [frames_4[-1]]
+                    
                     frames_4 = frames_4[1:]
                     state_stack = np.swapaxes(state_stack,0,2)
                     state_stack = np.swapaxes(state_stack,0,1)
-                    #print(state_stack.shape)
                     
                     state_buff.append(state_stack)
                     
@@ -713,11 +649,10 @@ class DQN_Agent():
                     
                     if True:#action_cont %4 == 0:
                         cur_action = self.env.action_space.sample()#take new action for next step
-                        #action_cont = 0
+                        
                 
                 
-                #total_reward += reward
-                #num_steps += 1
+                
                 cur_state = nextstate
 
                 if is_terminal or num_transition >= self.replay_memory.burn_in:
@@ -726,7 +661,6 @@ class DQN_Agent():
             
            
             
-        #print(len(self.replay_memory.memory),233323232)
         
     def burn_in_memory2(self):
         # Initialize your replay memory with a burn_in number of episodes / transitions. 
@@ -759,21 +693,14 @@ class DQN_Agent():
             
                 action_cont += 1
                 nextstate, reward, is_terminal, debug_info = self.env.step(cur_action)
-            
-                #reward = max(-1, min(reward, 1))
-            
-    
                 frames_4.append(rgb2gray(nextstate))
         
                 if len(frames_4) > 4:
                 
                     state_stack = np.asarray(frames_4[:-1])
-                    #frames_4 = [frames_4[-1]]
                     frames_4 = frames_4[1:]
                     state_stack = np.swapaxes(state_stack,0,2)
                     state_stack = np.swapaxes(state_stack,0,1)
-                    #print(state_stack.shape)
-                
                     state_buff.append(state_stack)
                 
                 
@@ -792,16 +719,10 @@ class DQN_Agent():
                                     
                     
                     if action_cont%1 == 0:
-                        #cur_action2 = self.greedy_policy(np.add(q_values[0], q_values2[0]))# A'
                         cur_action2 = self.epsilon_greedy_policy(np.add(q_values[0], q_values2[0]), epsilon = 0)
                         action_cont = 0
                     
-                    
-                    
-            
-            
-                #total_reward += reward
-                #num_steps += 1
+                  
                 cur_state = nextstate
 
                 if is_terminal or num_transition >= self.replay_memory.burn_in:
@@ -809,8 +730,6 @@ class DQN_Agent():
         
         
        
-        
-        #print(len(self.replay_memory.memory),233323232)
     
 
 
@@ -843,23 +762,19 @@ def run_random_policy(env):
     while True:
         
         nextstate, reward, is_terminal, debug_info = env.step(cur_action)
-        #reward = min(reward,1.0)
-        
         env.render()
-        
         frames_4.append(rgb2gray(nextstate))
             
         print(len(frames_4))
         if True and len(frames_4)> M:
             cur_action = env.action_space.sample()#take new action for next step
             state_stack = np.asarray(frames_4[:-1])
-            #frames_4 = [frames_4[-1]]
             frames_4 = frames_4[1:]
             state_stack = np.swapaxes(state_stack,0,2)
             state_stack = np.swapaxes(state_stack,0,1)
             print(state_stack.shape)
         
-        #print('score',reward)
+        
         
 
         total_reward += reward
@@ -878,9 +793,7 @@ def run_random_policy(env):
 def rgb2gray(rgb):
     
     rgb = cv2.resize(rgb,(input_dim,input_dim))
-    #pixel =np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
     color_img = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
-    
     return np.int8(color_img)
 
 
@@ -888,10 +801,6 @@ def main(args):
 
     args = parse_arguments()
     environment_name = args.env
-
-    #print(args.env, args.render, args.train, args.model_file)
-    
-
     # Setting the session to allow growth, so it doesn't allocate all GPU memory. 
     gpu_ops = tf.GPUOptions(allow_growth=True)
     config = tf.ConfigProto(gpu_options=gpu_ops)
@@ -901,18 +810,15 @@ def main(args):
     # Setting this as the default tensorflow session. 
     keras.backend.tensorflow_backend.set_session(sess)
     
-    #env = gym.make(environment_name)
-    #run_random_policy(env)
     
     
     agent = DQN_Agent(environment_name,render=args.render)
     
     
     if args.train>0:
-        #agent.burn_in_memory()
-        agent.burn_in_memory2()
+        agent.burn_in_memory()
+        #agent.burn_in_memory2()
         agent.train()
-        #agent.test_in_train()
     
     if args.model_file != None:
         agent.test(args.model_file)
